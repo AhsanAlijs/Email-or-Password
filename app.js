9ac0286bee5ec42a11151b2b79f33782172088b2
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-09-14 09:02:46 -0700

    Form Complete

---
+++ app.js
@@ -0,0 +1,77 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const result = document.getElementById("result")
+
+
+
+const arry = [];
+
+function render() {
+    result.innerHTML = ''
+    const user = {
+        Email: email.value,
+        Password: password.value
+    }
+    arry.push(user)
+
+    email.value = ""
+    password.value = ""
+    for (let i = 0; i < arry.length; i++) {
+        // console.log(arry[i]);
+        result.innerHTML += `<p>Email: ${arry[i].Email}</p>
+ <p>Password:${arry[i].Password}</p>
+ <button onclick="edit(${i})">Edit</button>
+ <button onclick="remove(${i})">Delete</button>
+ `
+      
+    }
+}
+function add(e) {
+    if(email.value === ""){
+        alert('Enter Email Password')
+    }else{
+        e.preventDefault()
+    render()
+    }
+    
+}
+
+
+
+
+function edit(index) {
+// console.log(index)
+result.innerHTML = ''
+
+const changeEmail = prompt("Change Emial",arry[index].Email)
+const changePassword = prompt("Change Password",arry[index].Password)
+arry[index].Email=changeEmail;
+arry[index].Password=changePassword;
+for (let i = 0; i < arry.length; i++) {
+    // console.log(arry[i]);
+    result.innerHTML += `<p>Email: ${arry[i].Email}</p>
+<p>Password:${arry[i].Password}</p>
+<button onclick="edit(${i})">Edit</button>
+<button onclick="remove(${i})">Delete</button>
+`
+}
+
+}
+
+function remove(index){
+    // console.log(index);
+result.innerHTML = ''
+
+    arry.splice(index,1)
+    for (let i = 0; i < arry.length; i++) {
+        // console.log(arry[i]);
+        result.innerHTML += `<p>Email: ${arry[i].Email}</p>
+    <p>Password:${arry[i].Password}</p>
+    <button onclick="edit(${i})">Edit</button>
+    <button onclick="remove(${i})">Delete</button>
+    `
+    }
+
+
+}
+
